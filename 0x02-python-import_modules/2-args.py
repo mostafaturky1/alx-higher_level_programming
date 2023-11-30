0734c152217de9d74a4c49af60895d6a983b2fe7
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2023-11-30 15:57:47 -0800

    “commit”

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(number))
-    for i in range (number):
+    for i in range(number):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
