e2037eab55a53817ef448ab4947de0127c545dd1
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2023-12-07 01:40:18 +0200

    “commit”

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted(a_dictionary)
-    for i in a_dictionary:
-        print("{}: {}".format(i, a_dictionary[i]))
+    for i, k in sorted(a_dictionary.items()):
+        print(i, k, sep=": ")
