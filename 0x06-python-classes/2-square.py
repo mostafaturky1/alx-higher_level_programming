f2489c9ca0d2010740172ab30fb126119dcb6aa0
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2023-12-20 01:28:05 +0200

    “final”

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 
 class Square:
-    """Represents a square with a given private size."""
+    """Represent a square."""
     def __init__(self, size=0):
         if size < 0:
             raise ValueError("size must be >= 0")
