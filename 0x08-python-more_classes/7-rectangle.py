03e5e52080798592ab54ddd5c4e6f8f8dbfbaadf
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-04 23:48:40 +0200

    “commit”

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -58,21 +58,6 @@ class Rectangle:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
-    def __str__(self):
-        """Return the printable representation of the Rectangle.
-
-        Represents the rectangle with the # character.
-        """
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        rect = []
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rect.append('#')
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
-
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Return the Rectangle with the greater area.
@@ -90,6 +75,21 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return (rect_1)
         return (rect_2)
+
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+
+        Represents the rectangle with the # character.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect = []
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rect.append('#')
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
