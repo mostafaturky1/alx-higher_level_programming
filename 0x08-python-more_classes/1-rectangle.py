710f5819341c971ba25564643625bc91f9fdec35
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-04 22:31:23 +0200

    “commit”

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,12 +4,20 @@
 
 class Rectangle:
     """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.__height = height
         self.__width = width
 
     @property
     def width(self):
+        """Get/set the width of the rectangle."""
         return self.__width
     
     @width.setter
@@ -21,6 +29,7 @@ class Rectangle:
         self.__width = value
     @property
     def height(self):
+        """Get/set the height of the rectangle."""
         return self.__height
     @height.setter
     def height(self, value):
