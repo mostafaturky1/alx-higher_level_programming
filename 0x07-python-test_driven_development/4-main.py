214275f35ea7e460b482333d421ff9b1542ba871
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-08 00:02:56 +0200

    “commit”

---
+++ 0x07-python-test_driven_development/4-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+print_square = __import__('4-print_square').print_square
+
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(-12.21)
+print("")
+
