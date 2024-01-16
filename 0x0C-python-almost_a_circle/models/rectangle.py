f397eecb3ed5e228cd87eb2465b28a4f65a18232
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-17 01:33:09 +0200

    “commit”

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,30 +1,10 @@
 #!/usr/bin/python3
-
+"""Defines a rectangle class."""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    A class representing a rectangle, inheriting from the Base class.
-
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        x (int): The x-coordinate of the rectangle.
-        y (int): The y-coordinate of the rectangle.
-        id (int): The identifier of the rectangle.
-
-    Methods:
-        area(): Calculate and return the area of the rectangle.
-        display(): Display the rectangle using '#' characters.
-        __str__(): Return a string representation of the rectangle.
-
-    Properties:
-        width (int): Get or set the width of the rectangle.
-        height (int): Get or set the height of the rectangle.
-        x (int): Get or set the x-coordinate of the rectangle.
-        y (int): Get or set the y-coordinate of the rectangle.
-    """
+    """Represent a rectangle."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
