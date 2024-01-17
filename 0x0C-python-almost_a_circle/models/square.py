0a8dc6a38a9ddc625447113ccb64fe32f7ba615b
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-18 01:04:59 +0200

    “commit”

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Initialize a Square instance."""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """
@@ -18,3 +19,26 @@ class Square(Rectangle):
             str: A string representation of the square.
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """Get or set the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        Args:
+            value (int): The size value to set.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.siz = value
