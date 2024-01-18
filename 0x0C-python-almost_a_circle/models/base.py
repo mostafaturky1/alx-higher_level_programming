390dcc8216f050e9124341c5257a9cf1f52f4ff2
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-01-18 02:29:03 +0200

    “commit”

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,6 +29,6 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """to json string to returns the JSON string representation"""
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries is None:
             return []
         return json.dumps(list_dictionaries)
