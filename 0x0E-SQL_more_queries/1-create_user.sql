a05acb5e3f5eda4bf12f49ac4b135a47dad5d847
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-14 22:00:07 +0200

    “commit”

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
 -- script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON MySQL.* TO * . * TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS user_0d_1@localhost 
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANTS ALL PRIVILEGES 
+ON * . * TO user_0d_1@localhost;
