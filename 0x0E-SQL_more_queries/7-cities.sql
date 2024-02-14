69519a6031872dfeec9746c3c61bd196e1e89eb1
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-14 23:20:19 +0200

    “commit”

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
-	state_id INT,
+	`state_id` INT,
 	`name` VARCHAR(256),
 	FOREIGN KEY(state_id)
 	REFERANCES states(id)
