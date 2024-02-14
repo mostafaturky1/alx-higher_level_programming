a89c41bf126d5518e68e1603d95811a310607f2c
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-14 22:18:22 +0200

    “commit”

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost;
-GRANT SELECT 
-ON hbtn_0d_2.*
+GRANT SELECT
+ON DATABASE hbtn_0d_2
 TO user_0d_2@localhost;
