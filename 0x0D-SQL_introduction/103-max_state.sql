341e313c08db04f1765ecf739634af854a993b0d
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-02-14 19:10:29 +0200

    “commit”

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- script that displays the max temperature of each state (ordered by State name).
 SELECT `state`, max(`value`) as `max_temp`
-FROM `temperature`
+FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state` DESC;
