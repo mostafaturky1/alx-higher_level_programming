181025c15d207ed248b92da867fcdd2311bc85a9
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2024-03-13 02:45:02 +0200

    “commit”

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require("./4-rectangle");
+const square = require("./5-square");
 
-class Square extends Rectangle {
-  constructor(size) {
-    super(size, size);
-  }
+class Square extends square {
 
   charPrint(c) {
     if (c === undefined) {
