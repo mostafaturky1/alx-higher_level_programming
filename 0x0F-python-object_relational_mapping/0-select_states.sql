395ff2eceb8fa1d3dce332f43467e6d6bf4b9a50
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2024-03-17 00:03:15 +0000

    “commit”

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -1,7 +1,4 @@
 -- Create states table in hbtn_0e_0_usa with some data
-
-DROP DATABASE IF EXISTS hbtn_0e_0_usa;
-
 CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
 USE hbtn_0e_0_usa;
 CREATE TABLE IF NOT EXISTS states ( 
