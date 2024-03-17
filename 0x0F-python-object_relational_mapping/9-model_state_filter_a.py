ec2ed0dab888a6b4077da303356d5622d93dad73
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2024-03-17 20:29:06 +0000

    “commit”

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    states = session.query(State).\
+        filter((State.name.like('%a%'))).order_by(State.id)
+    if (states is None):
+        print("Nothing")
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
