d7ce1a328880655a5aa6715804b82690f770dc8d
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2024-03-17 19:48:02 +0000

    “commit”

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 session = sessionmaker(bind=engine)
 session = session()
 
-states = session.query(State)
+states = session.query(State).order_by(State.id)
 
 for state in states:
     print("{}: {}".format(state.id, state.name))
