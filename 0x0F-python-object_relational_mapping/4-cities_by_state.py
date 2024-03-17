89cc2d6b305ef28ecbed8bb25a0f29ec048c352c
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2024-03-17 04:35:01 +0000

    “commit”

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = conn.cursor()
+
+    result = cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                         cities, states WHERE cities.state_id = states.id """)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
