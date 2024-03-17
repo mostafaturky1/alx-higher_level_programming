82bed636f4604f62ac9dad99e3355abf92f8c6e2
mostafaturky1 <mostafa.mohamed.chp@gmail.com>
2024-03-17 20:03:20 +0000

    “commit”

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,10 +14,10 @@ if __name__ == "__main__":
                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-session = sessionmaker(bind=engine)
-session = session()
+    session = sessionmaker(bind=engine)
+    session = session()
 
-states = session.query(State).order_by(State.id)
+    states = session.query(State).order_by(State.id)
 
-for state in states:
-    print("{}: {}".format(state.id, state.name))
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
